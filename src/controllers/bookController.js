d1fcd63716e68c31cffc2a44d02d905ef02905dc
Mani <manimoy7@gmail.com>
2022-08-24 19:16:03 +0530

    middleware-timestamp

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,152 +1,79 @@
+const { count } = require("console")
+const BookModel = require("../models/bookModel")
 
-const authorModel = require("../models/authorModel")
-const userModel = require("../models/userModels")
+const createBook= async function (req, res) {
+    let data= req.body
 
-
-const creatBooks = async function(req,res){
-    let data = req.body
-    let saveData = await userModel.create(data)
-    res.send({msg: saveData})
-
-}
-
-const authorList = async function(req,res){
-    let data = req.body
-    let saveAuthorData = await authorModel.create(data)
-    res.send({msg: saveAuthorData})
-}
-
-const listBooks = async function(req, res){
-    let findauthor = await authorModel.find({author_name:"Chetan Bhagat"})
-    let findbook = await userModel.find({ author_id : {$eq : findauthor[0].author_id}})
-    res.send({msg : findbook})
-}
-
-const updatebook = async function(req,res){
-    let bookprice = await userModel.findOneAndUpdate({book_name : "Two States" },{$set : {price: 100}},{new:true})
-    let updatedprice = bookprice.price;
-    let authorupdate = await authorModel.find({author_id: {$eq : bookprice.author_id}}).select({author_name:1,_id:0})
-    res.send({authorupdate , updatedprice})
-
-}
-
-const bookrange = async function(req,res){
-    let range = await userModel.find({price:{$gt:50,$lte:100}})
-    let a = range.map(x=>x.author_id);
-    let newrange = await authorModel.find({author_id:{$in:a}}).select({author_name:1,_id:0})
-    res.send(newrange)
+    let savedData= await BookModel.create(data)
+    res.send({msg: savedData})
 }
 
 
 
-module.exports.creatBooks = creatBooks;
-module.exports.authorList = authorList;
-module.exports.listBooks = listBooks;
-module.exports.updatebook = updatebook;
-module.exports.bookrange  = bookrange ;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-const createBooklist = async function(req,res){
-    
-    let data = req.body
-    let saveData = await userModel.create(data)
-
-    res.send({msg: saveData})
-
+const getBooksData = async function (req, res) {
+    let allBooks = await BookModel.find({ authorName: "HO" })
+    console.log(allBooks)
+    if (allBooks.length > 0) res.send({ msg: allBooks, condition: true })
+    else res.send({ msg: "No books found", condition: false })
 }
 
-const getBookList = async function(req,res){
 
-    // let allBooks = await userModel.find({bookName:1, AuthorName:1})
-    // res.send({msg: allBooks})
-
-    let allUser =await userModel.find({},{bookName:1 , AuthorName:1 , _id:0})
-    res.send({msg: allUser})
-}
-
-const getBooksInYear = async function(req,res){
-    let year = req.query.year
-    let books = await userModel.find({year: year})
-    res.send({data: books})
-}
-
-const getParticularBooks = async function(req,res){
-    let input = req.query.bookName
-    let reg = /[A-z]/g
-    let books = await userModel.find({bookName: input}).match(reg)
-    res.send({data: books})
-}
-
-const getXINRBooks = async function(req,res){
-    let books =await userModel.find({$or:[{indianPrice: 100},{indianPrice: 200},{indianPrice: 500}]}).select({bookName: 1})
-    res.send({data: books})
-
-}
-
-const getRandomBooks = async function(req,res){
-    let books = await userModel.find({$or: [{stockAvailable: true,totalPages:{$gt: 500}}]})
-    res.send({data: books})
-
-}
-
-// const updateBooks = async function(req,res){
-//     let allBooks = await userModel.updateMany(
-//         {AuthorName: "jadu"},
-//         {$set: {totalPages:1000000}}
-//     )
-//     res.send({msg: allBooks})
-// }
-
-
-// this is from user end updating
-
-// const updateBooks = async function(req,res){
-//     let data = req.body
-//     let allBooks = await userModel.updateMany(
-//         {AuthorName: "jadu"},
-//         {$set: data}
-//     )
-//     res.send({msg: allBooks})
-// }
-
-// const updateBooks = async function(req,res){
-//     let data = req.body
-//     let allBooks = await userModel.findOneAndUpdate(
-//         {AuthorName: "jadu"},
-//         {$set: data}
-//     )
-//     res.send({msg: allBooks})
-// }
-
-const updateBooks = async function(req,res){
-    let data = req.body
-    let allBooks = await userModel.findOneAndUpdate(
-        {AuthorName: "jadu"},
-        {$set: data},
-        {new: true, upsert: true},
+const updateBooks = async function (req, res) {
+    let data = req.body // {sales: "1200"}
+    // let allBooks= await BookModel.updateMany( 
+    //     { author: "SK"} , //condition
+    //     { $set: data } //update in data
+    //  )
+    let allBooks = await BookModel.findOneAndUpdate(
+        { authorName: "ABC" }, //condition
+        { $set: data }, //update in data
+        { new: true, upsert: true } ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT
     )
-    res.send({msg: allBooks})
+
+    res.send({ msg: allBooks })
+}
+
+const deleteBooks = async function (req, res) {
+    // let data = req.body 
+    let allBooks = await BookModel.updateMany(
+        { authorName: "FI" }, //condition
+        { $set: { isDeleted: true } }, //update in data
+        { new: true } ,
+    )
+
+    res.send({ msg: allBooks })
+}
+
+
+
+const totalSalesPerAuthor = async function (req, res) {
+    // let data = req.body 
+    let allAuthorSales = await BookModel.aggregate(
+        [
+            { $group: { _id: "$authorName", totalNumberOfSales: { $sum: "$sales" } } },
+            { $sort: { totalNumberOfSales: -1 } }
+        ]
+    )
+
+    res.send({ msg: allAuthorSales })
 }
 
 
 
 
-module.exports.updateBooks = updateBooks;
-module.exports.createBooklist = createBooklist ;
-module.exports.getBookList = getBookList ;
-module.exports.getBooksInYear = getBooksInYear;
-module.exports.getParticularBooks = getParticularBooks;
-module.exports.getXINRBooks = getXINRBooks;
-module.exports.getRandomBooks = getRandomBooks;
+// CRUD OPERATIONS:
+// CREATE
+// READ
+// UPDATE
+// DELETE
+
+
+
+module.exports.createBook = createBook
+module.exports.getBooksData = getBooksData
+module.exports.updateBooks = updateBooks
+module.exports.deleteBooks = deleteBooks
+module.exports.totalSalesPerAuthor = totalSalesPerAuthor
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,328 +1,113 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
-const bookController = require("../controllers/bookController")
-
+const time = require('express-timestamp')
 const router = express.Router();
-const userModel = require("../models/userModels")
+var app = express()
+// const UserModel= require("../models/userModel.js")
+const UserController = require("../controllers/userController")
+const BookController = require("../controllers/bookController")
+const commonMW = require("../middlewares/commonMiddlewares")
 
-
-router.post("/creat-book",bookController.creatBooks)
-
-router.post("/createAuthorList",bookController.authorList)
-
-router.get("/listBooks",bookController.listBooks)
-
-router.get("/updatebook",bookController.updatebook)
-
-router.get("/bookrange",bookController.bookrange)
-
-
-
-
-
-
-
-
-
-
-router.post("/create-book",bookController.createBooklist )
-
-
-router.get("/book-list",bookController.getBookList)
-
-router.post("/get-Books-In-Year",bookController.getBooksInYear)
-
-router.post("/get-Particular-Books",bookController.getParticularBooks)
-
-router.get("/get-XINR-Books",bookController.getXINRBooks)
-
-router.get("/get-Random-Books",bookController.getRandomBooks)
-
-router.post("/update",bookController.updateBooks)
-
-// })
-
-
-
-
-let persons = [
-    {
-        name : "pk",
-        age : 10 ,
-        votingStatus : false
-
-    },
-    {
-        name : "sk",
-        age : 20 ,
-        votingStatus : false
-
-    },
-    {
-        name : "aa",
-        age : 70 ,
-        votingStatus : false
-
-    },
-    {
-        name : "sc",
-        age : 5 ,
-        votingStatus : false
-
-    },
-    {
-        name : "ho",
-        age : 40 ,
-        votingStatus : false
-
-    },
-]
-
-router.post('/getvooting-status', function(req,res){
-    let votingAge = req.query.age
-    let eligibalePerson = []
-    for (let i = 0; i<persons.length; i++){
-        if(persons[i].age > votingAge){
-            persons[i].votingStatus = true;
-            eligibalePerson.push(persons[i])
-        }
-    }
-    res.send({persons:eligibalePerson,status:true})
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
 })
 
 
 
 
-
-
-
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha', 'Akash', 'Pritesh'])
-    console.log('The first element received from underscope function is ' + firstElement)
-    res.send('My first ever api!')
-});
-
-router.get("/myy-query-param",function(req,res){
-
-    let marks = req.query.marks
-    let result = marks > 40 ? "pass" : "fail"
-    res.send({data : result, status : true})
-
-})
-
-//filter out all numbers are gretter then input
-
-let myArr = [12,55,78,241,69,14,44]
-router.post("/filter",function(req,res){
-
-
-    let input = req.query.input
-    let finalArr = myArr.filter(ele => ele > input)
-    
-    // let num = req.query.myArr
-    // const result = myArr.filter(myArr => myArr.length > i);
-    res.send({data:finalArr, status :true})
-})
+router.post("/createBook", BookController.createBook)
 
 
 
 
+// router.post("/createUser", UserController.createUser  )
+// router.get("/getUsersData", UserController.getUsersData)
+
+
+const mid1 = function (req, res, next) {
+    console.log("Hi I am a middleware named Mid1")
+    // logic
+
+    // let loggedIn = true
+
+    // if (loggedIn== true) { 
+    //     //console.log( "OK LOGGED IS IS TRUE NOW")
+    //     next ()
+    // }
+    // else {
+    //     //res.send ("Please login or register")
+    // }
+    console.log(req.path);
+    // app.use(time.init)
+    // console.log(time)
+
+    var ip = require("ip");
+    console.log(ip.address());
+
+    // var timestamp = Number(new Date());
+    // console.log(timestamp);
+
+    // const dateObject = new Date();
+    // current date
+    // adjust 0 before single digit date
+    // const date = (`0 ${dateObject.getDate()}`).slice(-2);
+    // console.log(date)
+
+    const dateObject = new Date();
+    // current date
+    // adjust 0 before single digit date
+    const date = (`0 ${dateObject.getDate()}`).slice(-2);
+
+    // current month
+    const month = (`0 ${dateObject.getMonth() + 1}`).slice(-2);
+
+    // current year
+    const year = dateObject.getFullYear();
+
+    // current hours
+    const hours = dateObject.getHours();
+
+    // current minutes
+    const minutes = dateObject.getMinutes();
+
+    // current seconds
+    const seconds = dateObject.getSeconds();
+
+    // prints date & time in YYYY-MM-DD HH:MM:SS format
+    console.log(`${year}-${month}-${date} ${hours}:${minutes}:${seconds}`);
+
+    next()
+}
+
+router.get('/homePage', mid1, UserController.basicCode)
 
 
 
 
-let players = [
-    {
-        "name": "manish",
-        "dob": "1/1/1995",
-        "gender": "male",
-        "city": "jalandhar",
-        "sports":
-            [
-                "swimming"
-            ]
-    },
-    {
-        "name": "gopal",
-        "dob": "1/5/1996",
-        "gender": "male",
-        "city": "delhi",
-        "sports":
-            [
-                "football"
-            ]
-    },
-    {
-        "name": "lockes",
-        "dob": "5/7/1999",
-        "gender": "male",
-        "city": "UP",
-        "sports":
-            [
-                "busketball"
-            ]
-
-    }
-]
-
-router.post('/players', function(req, res){
-
-    let newPlayer = req.body
-    let newPlayersName = newPlayer.name
-    let isNameRepeted = false //flag
-
-    for(let i=0; i < players.length; i++){
-        if(players[i].name == newPlayersName){
-            isNameRepeted = true;
-            break;
-        }
-    }
-        if (isNameRepeted){
-            res.send("Player is already exist") 
-        }
-        else{
-            players.push(newPlayer)
-            res.send(players)
-        }
-    //res.send( { data : players , status : true} )
-});
 
 
-router.post('/test-post-4', function (req, res) {
-    let arr = [12, "FunctionUp"]
-    let ele = req.body.element
-    arr.push(ele)
 
-    res.send({ msg: arr, status: true })
-})
+// // e.g. restricted and open-to-all API's can be handled like below now:
+
+// router.get('/profileDetails', mid1, UserController.profileDetails)
+// router.get('/friendList', mid1, UserController.friendList)
+// router.get('/changePassword', mid1, UserController.changePassword)
+
+// router.get('/termsAndConditions',  UserController.termsAndConditions)
+// router.get('/register',  UserController.register)
 
 
-router.get("/movies/:indexNumber", function (req, res) {
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    //check index value. less than 0 or greater than array (length - 1) are not valid
-    if (movieIndex < 0 || movieIndex >= movies.length) {
-        //if the index is invalid send an error message
-        return res.send('The index value is not correct, Please check the it')
-    }
 
-    //if the index was valid send the movie at that index in response
-    let requiredMovie = movies[movieIndex]
-    res.send(requiredMovie)
-})
 
-router.get("/numbers-chk", function (req, res) {
-    let arr = [1, 2, 4, 5, 6, 7, 8]
-    function number(arr) {
-        for (let i = 0; i < arr.length; i++) {
-            if (i + 1 != arr[i]) {
-                return arr[i] - 1
-            } else {
 
-            }
-        }
-    }
-    let result = number(arr)
-    res.send(result.toString())
-})
+router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
-router.get("/Second-number", function (req, res) {
-    const findMissing = num => {
-        const max = Math.max(...num);
-        const min = Math.min(...num);
-        const missing = []
 
-        for (let i = min; i <= max; i++) {
-            if (!num.includes(i)) {
-                missing.push(i);
-            }
-        }
-        res.send(missing)
-    }
 
-    findMissing([1, 2, 6]);
-})
+// router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
+// router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
+// router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
 
-router.get("/shoes", function (req, res) {
-    let queryParams = req.query
-    let brand = queryParams.brand
-    res.send("dummy response")
-})
 
-// uses query params
-router.get('/candidates', function (req, res) {
-    console.log('Query paramters for this request are ' + JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is ' + state)
-    console.log('Gender is ' + gender)
-    console.log('District is ' + district)
-    let candidates = ['Akash', 'Suman']
-    res.send(candidates)
-})
 
-// use path param
-router.get('/candidates/:canidatesName', function (req, res) {
-    console.log('The request objects is ' + JSON.stringify(req.params))
-    console.log('Candidates name is ' + req.params.canidatesName)
-    res.send('Done')
-})
-
-router.get("/films", function (req, res) {
-    const films = [{
-        "id": 1,
-        "name": "The Shining"
-    }, {
-        "id": 2,
-        "name": "Incendies"
-    }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-    }, {
-        "id": 4,
-        "name": "Finding Nemo"
-    }]
-    //send all the films
-    res.send(films)
-})
-
-router.get("/films/:filmId", function (req, res) {
-    const films = [{
-        "id": 1,
-        "name": "The Shining"
-    }, {
-        "id": 2,
-        "name": "Incendies"
-    }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-    }, {
-        "id": 4,
-        "name": "Finding Nemo"
-    }]
-
-    let filmId = req.params.filmId
-
-    //iterate all the films
-    //search for a film whose id matches with the id recevied in request
-    for (let i = 0; i < films.length; i++) {
-        let film = films[i]
-        if (film.id == filmId) {
-            //if there is a match return the response from here
-            return res.send(film)
-        }
-    }
-
-    //if there is no match give an error response
-    res.send("The film id doesn't match any movie")
-})
 
 module.exports = router;
-// adding this comment for no reason
