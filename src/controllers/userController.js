2d97dd2dfa4fdccd92ff3d682d36025105595697
Mani <manimoy7@gmail.com>
2022-08-30 19:07:15 +0530

    httpStatus

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,91 +3,112 @@ const userModel = require("../models/userModel");
 //crate user ***************************************************************************************************
 
 const createUser = async function (req, res) {
-    let data = req.body;
-    let savedData = await userModel.create(data);
-    console.log(req.newAtribute);
-    res.send({ msg: savedData });
+    try {
+        let data = req.body;
+        let savedData = await userModel.create(data);
+        console.log(req.newAtribute);
+        res.send({ msg: savedData });
+    } catch (err) { return res.status(100).send({ msg: "Server Error" }) }
 };
 
 //login user ***************************************************************************************************
 const loginUser = async function (req, res, next) {
-    let userName = req.body.emailId;
-    let password = req.body.password;
-    let user = await userModel.findOne({ emailId: userName, password: password });
-    if (!user) {
-        return res.send({ status: false, msg: "username or the password is not corerct", });
-    } else if (user.isDeleted === true) {
-        res.send("this account is deleted you can't log in, please create new account ")
-    } else {
-        next()
-    }
+
+    try {
+        let userName = req.body.emailId;
+        let password = req.body.password;
+        let user = await userModel.findOne({ emailId: userName, password: password });
+        if (!user) {
+            return res.send({ status: false, msg: "username or the password is not corerct", });
+        } else if (user.isDeleted === true) {
+            res.send("this account is deleted you can't log in, please create new account ")
+        } else {
+            next()
+        }
+    } catch (err) { return res.status(401).send({ msg: "Server Error" }) }
 }
 
 
 // getuserData ******************************************************************************************
 const getUserData = async function (req, res) {
 
-    let userId = req.params.userId;
-    let userDetails = await userModel.findById(userId);
-    if (!userDetails) {
-        return res.send({ status: false, msg: "No such user exists" });
-    } else if (userDetails.isDeleted == true) {
-        res.send("this user is deleted from our databse you can't find")
-    } else {
-        res.send({ status: true, data: userDetails });
-    }
+    try {
+        let userId = req.params.userId;
+        let userDetails = await userModel.findById(userId);
+        if (!userDetails) {
+            return res.send({ status: false, msg: "No such user exists" });
+        } else if (userDetails.isDeleted == true) {
+            res.send("this user is deleted from our databse you can't find")
+        } else {
+            res.send({ status: true, data: userDetails });
+        }
+
+    } catch (err) { return res.status(400).send({ msg: "Server Error" }) }
+
 };
 
 // post message ************************************************************************************
 const postMessage = async function (req, res) {
-    let message = req.body.message
-    let user = await userModel.findById(req.params.userId)
-    if (!user) {
-        return res.send({ status: false, msg: 'No such user exists' })
-    } else if (user.isDeleted == true) {
-        res.send("this user is deleted from our database you can't post a message")
-    } else {
-        let updatedPosts = user.posts
-        //add the message to user's posts
-        updatedPosts.push(message)
-        let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: updatedPosts }, { new: true })
-        //return the updated user document
-        return res.send({ status: true, data: updatedUser })
-    }
+
+    try {
+        let message = req.body.message
+        let user = await userModel.findById(req.params.userId)
+        if (!user) {
+            return res.send({ status: false, msg: 'No such user exists' })
+        } else if (user.isDeleted == true) {
+            res.send("this user is deleted from our database you can't post a message")
+        } else {
+            let updatedPosts = user.posts
+            //add the message to user's posts
+            updatedPosts.push(message)
+            let updatedUser = await userModel.findOneAndUpdate({ _id: user._id }, { posts: updatedPosts }, { new: true })
+            //return the updated user document
+            return res.send({ status: true, data: updatedUser })
+
+        }
+
+    } catch (err) { return res.status(400).send({ msg: "Server Error" }) }
 }
 
 // updateUser ************************************************************************************************************
+
 const updateUser = async function (req, res) {
+    try {
+        let userId = req.params.userId;
+        let user = await userModel.findById(userId);
+        //Return an error if no user with the given id exists in the db
+        if (!user) {
+            return res.send("No such user exists");
+        } else if (user.isDeleted == true) {
+            res.send("you can't update this user , this user is deleted from our database")
+        } else {
 
-    let userId = req.params.userId;
-    let user = await userModel.findById(userId);
-    //Return an error if no user with the given id exists in the db
-    if (!user) {
-        return res.send("No such user exists");
-    } else if (user.isDeleted == true) {
-        res.send("you can't update this user , this user is deleted from our database")
-    } else {
-
-        let userData = req.body;
-        let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
-        res.send({ status: true, data: updatedUser });
-    }
+            let userData = req.body;
+            let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
+            res.send({ status: true, data: updatedUser });
+        }
+    } catch (err) { return res.status(500).send({ msg: "Server Error" }) }
 };
 
 //deleteUser***************************************************************************************
 const deleteUser = async function (req, res) {
 
-    let userId = req.params.userId;
-    let user = await userModel.findById(userId);
-    //Return an error if no user with the given id exists in the db
-    if (!user) {
-        return res.send("No such user exists");
-    } else if (user.isDeleted == true) {
-        res.send("this account  is already deleted, please create new account to delte ")
-    } else {
-        let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, { isDeleted: true }, { new: true });
-        res.send({ status: true, dl: "user deleted successfully", data: updatedUser });
-    }
+    try {
+        let userId = req.params.userId;
+        let user = await userModel.findById(userId);
+        //Return an error if no user with the given id exists in the db
+        if (!user) {
+            return res.send("No such user exists");
+        } else if (user.isDeleted == true) {
+            res.send("this account  is already deleted, please create new account to delte ")
+        } else {
+            let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, { isDeleted: true }, { new: true });
+            res.send({ status: true, dl: "user deleted successfully", data: updatedUser });
+        }
+
+    } catch (err) { return res.status(404).send({ msg: "Server Error" }) }
+
+
 };
 
 
